b7fa2b5c8a1e908cc488c778890afab0462c62cf
Gabriel Camarena Gomez <gcamarenagomez@gcamarenagomez@salesforce.com>
2018-04-14 16:58:23 -0500

    Basecamp Mexico

--- handler.js
+++ handler.js
@@ -9,6 +9,7 @@ exports.SearchBags = (slots, session, response) => {
 
 exports.AnswerPNR = (slots, session, response) => {
     if(session.attributes.stage === 'ask_pnr'){
+        console.log("PNR: " + slots);
         salesforce.findCase({pnr: slots.pnr.value})
         .then(cases => {
             session.attributes.cases = cases;
